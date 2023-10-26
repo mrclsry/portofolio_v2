1f4cdb7a4cb9459369027d6b6174ed3af00dc3c4
Marcelius Surya Wijaya <89804627+mrclsry@users.noreply.github.com>
2023-10-26 23:00:32 +0700

    Update_Bootstrap

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const body = document.querySelector('body'),
+  sidebar = body.querySelector('nav'),
+  toggle = body.querySelector('.toggle'),
+  modeSwitch = body.querySelector('.toggle-switch'),
+  modeText = body.querySelector('.mode-text');
+
+modeSwitch.addEventListener('click', () => {
+  body.classList.toggle('dark');
+
+  if (body.classList.contains('dark')) {
+    modeText.innerText = 'Light mode';
+  } else {
+    modeText.innerText = 'Dark mode';
+  }
+});
